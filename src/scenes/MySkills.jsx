aff5662c21d6d91207e192f1f6ae87c244f56cae
Xander <xandernickita318@gmail.com>
2023-04-16 20:20:16 -0400

    second push

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,20 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMG SECT */}
+      <div></div>
+    </section>
+  );
+};
+
+{
+  /* START AT 1:30:50 */
+}
+
+export default MySkills;
