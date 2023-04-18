be524041ba75ceba9b42973967a09395a448bb8c
Xander <xandernickita318@gmail.com>
2023-04-18 15:38:20 -0400

    style update for footer

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -1,21 +1,24 @@
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
-	return (
-		<footer className="h-64 bg-red pt-10">
-			<div className="w-10/12 mx-auto">
-				<SocialMediaIcons />
-				<div className="md:flex justify-center md:justify-between text-center ">
-					<p className="font-playfair font-semibold text-2xl text-yellow">
-						JANE ESPER
-					</p>
-					<p className="font-playfair text-md text-yellow">
-						©2022 DAEDALUS DESIGNS. All Rights Reserved.
-					</p>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="h-64 bg-ribcolororange pt-10">
+      <div className="w-10/12 mx-auto">
+        <SocialMediaIcons />
+        <div className="md:flex justify-center md:justify-between text-center ">
+          <p className="font-playfair font-semibold text-2xl text-bluebody">
+            JANE ESPER
+          </p>
+          <p className="font-playfair text-md text-bluebody">
+            ©2022 DAEDALUS DIGITAL SOLUTIONS
+          </p>
+          <p className="font-playfair text-md text-bluebody">
+            All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
