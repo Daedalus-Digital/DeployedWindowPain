7aca0652364cd92512c5582f40aa7bf266aa7877
Xander <xandernickita318@gmail.com>
2023-04-18 17:31:28 -0400

    changed title text

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -20,7 +20,7 @@ const MySkills = () => {
           }}
         >
           <p className="font-playfair font-semibold text-4xl mb-5">
-            OUR <span className="text-ribcolororange">SKILLS</span>
+            OUR <span className="text-ribcolororange">EXPERIENCE</span>
           </p>
           <LineGradient width="w-1/3" />
           <p className="mt-10 mb-7">
