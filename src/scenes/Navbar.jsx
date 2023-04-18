8cef645faecd27dd2c390bca6c1dabf50caac8a2
Xander <xandernickita318@gmail.com>
2023-04-18 14:43:50 -0400

    Updated styles, added functionality. "DDS" text is clickable now

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -3,110 +3,114 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
 const Link = ({ page, selectedPage, setSelectedPage }) => {
-	const lowerCasePage = page.toLowerCase();
-	return (
-		<AnchorLink
-			className={`${selectedPage === lowerCasePage ? "text-ribcolororange" : ""}
-                hover:text-yellow transition duration-450`}
-			href={`#${lowerCasePage}`}
-			onClick={() => setSelectedPage(lowerCasePage)}
-		>
-			{page}
-		</AnchorLink>
-	);
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-white" : ""}
+               text-white hover:text-ribcolororange transition duration-450`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
 };
 
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
-	const [isMenuToggled, setIsMenuToggled] = useState(false);
-	const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-	const navbarBackground = isTopOfPage ? "" : "bg-ribcolororange";
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-bluebody";
 
-	return (
-		<nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
-			<div className="flex items-center justify-between mx-auto w-5/6">
-				<h4 className="font-playfair text-3xl font-bold">DDS</h4>
+  return (
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <AnchorLink href="#home" onClick={() => setSelectedPage("home")}>
+          <h4 className="font-playfair text-3xl font-bold text-ribcolororange cursor-pointer hover:opacity-90">
+            DDS
+          </h4>
+        </AnchorLink>
 
-				{/* DESKTOP NAVBAR */}
-				{isAboveSmallScreens ? (
-					<div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-						<Link
-							page="Home"
-							selectedPage={selectedPage}
-							setSelectedPage={setSelectedPage}
-						/>
-						<Link
-							page="Our Skills"
-							selectedPage={selectedPage}
-							setSelectedPage={setSelectedPage}
-						/>
-						<Link
-							page="Our Projects"
-							selectedPage={selectedPage}
-							setSelectedPage={setSelectedPage}
-						/>
-						<Link
-							page="Testimonials"
-							selectedPage={selectedPage}
-							setSelectedPage={setSelectedPage}
-						/>
-						<Link
-							page="Contact Us"
-							selectedPage={selectedPage}
-							setSelectedPage={setSelectedPage}
-						/>
-					</div>
-				) : (
-					<button
-						className="rounded-full bg-orange-500 p-2"
-						onClick={() => setIsMenuToggled(!isMenuToggled)}
-					>
-						<img alt="menu" src="../assets/menu-icon.svg" />
-					</button>
-				)}
+        {/* DESKTOP NAVBAR */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-orange-500 p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img alt="menu" src="../assets/menu-icon.svg" />
+          </button>
+        )}
 
-				{/* MOBILE NAV POPUP */}
-				{!isAboveSmallScreens && isMenuToggled && (
-					<div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
-						{/* CLOSE ICON */}
-						<div className="flex justify-end p-12">
-							<button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-								<img alt="close-icon" src="../assets/close-icon.svg" />
-							</button>
-						</div>
+        {/* MOBILE NAV POPUP */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-bluebody w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src="../assets/close-icon.svg" />
+              </button>
+            </div>
 
-						{/* MENU ITEMS */}
-						<div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
-							<Link
-								page="Home"
-								selectedPage={selectedPage}
-								setSelectedPage={setSelectedPage}
-							/>
-							<Link
-								page="Our Skills"
-								selectedPage={selectedPage}
-								setSelectedPage={setSelectedPage}
-							/>
-							<Link
-								page="Our Projects"
-								selectedPage={selectedPage}
-								setSelectedPage={setSelectedPage}
-							/>
-							<Link
-								page="Testimonials"
-								selectedPage={selectedPage}
-								setSelectedPage={setSelectedPage}
-							/>
-							<Link
-								page="Contact Us"
-								selectedPage={selectedPage}
-								setSelectedPage={setSelectedPage}
-							/>
-						</div>
-					</div>
-				)}
-			</div>
-		</nav>
-	);
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
